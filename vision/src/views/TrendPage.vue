6e5bed091fbdc68ce17222262bbee4a0aa19a610
Wayne1180 <xutenglongxutenglong@outlook.com>
2022-10-18 09:52:05 +0800

    完成了商家销量趋势（折线图）

---
+++ vision/src/views/TrendPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="com-page">
+    <trend></trend>
+  </div>
+</template>
+
+<script>
+import Trend from "@/components/Trend";
+export default {
+  components: {
+    trend: Trend,
+  },
+};
+</script>
+
+<style>
+</style>
