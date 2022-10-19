e345539dc1a4c814ddd67326f1db48882bd412a8
Wayne1180 <xutenglongxutenglong@outlook.com>
2022-10-19 11:46:29 +0800

    完成了库存与销量模块（饼图）

---
+++ vision/src/views/StockPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="com-page">
+    <stock></stock>
+  </div>
+</template>
+
+<script>
+import Stock from "@/components/Stock";
+export default {
+  components: {
+    stock: Stock,
+  },
+};
+</script>
+
+<style>
+</style>
